ed91117a55f168cec95a6c00a1d4686064e305a7
Preethi V <preethivijay0706@gmail.com>
2023-10-05 23:34:01 +0530

    first commit

---
+++ Pattern.java
@@ -0,0 +1,107 @@
+import java.util.Scanner;
+
+public class Pattern 
+{
+    public static void main(String[] args) 
+    {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the digit: ");
+        int n = sc.nextInt();
+        for(int i=1; i<n;i++)
+        {
+            for(int j=i; j<=n;j++)           //decreasing pattern
+            {
+                System.out.print("  ");
+            }
+            for(int j=1;j<=i;j++)            //increasing pattern
+            {
+                System.out.print("* ");
+            }
+            for(int j=1;j<i;j++)
+            {
+                System.out.print("* ");
+            }
+            for(int j=i; j<=n;j++)        
+            {
+                System.out.print("  ");
+            }
+            for(int j=i; j<n;j++)        
+            {
+                System.out.print("  ");
+            }
+            for(int j=1;j<=i;j++)
+            {
+                System.out.print("* ");
+            }
+            for(int j=1;j<i;j++)
+            {
+                System.out.print("* ");
+            }
+            for(int j=i; j<=n;j++)        
+            {
+                System.out.print("  ");
+            }
+            for(int j=i; j<n;j++)        
+            {
+                System.out.print("  ");
+            }
+            for(int j=1;j<=i;j++)
+            {
+                System.out.print("* ");
+            }
+            for(int j=1;j<i;j++)
+            {
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+        for(int i=1; i<=n;i++)
+        {
+            for(int j=1; j<=i;j++)
+            {
+                System.out.print("  ");
+            }
+            for(int j=i;j<=n;j++)
+            {
+                System.out.print("* ");
+            }
+            for(int j=i;j<n;j++)
+            {
+                System.out.print("* ");
+            }
+            for(int j=1; j<=i;j++)
+            {
+                System.out.print("  ");
+            }
+            for(int j=1; j<i;j++)
+            {
+                System.out.print("  ");
+            }
+            for(int j=i;j<=n;j++)
+            {
+                System.out.print("* ");
+            }
+            for(int j=i;j<n;j++)
+            {
+                System.out.print("* ");
+            }
+            for(int j=1; j<=i;j++)
+            {
+                System.out.print("  ");
+            }
+            for(int j=1; j<i;j++)
+            {
+                System.out.print("  ");
+            }
+            for(int j=i;j<=n;j++)
+            {
+                System.out.print("* ");
+            }
+            for(int j=i;j<n;j++)
+            {
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+    }
+}
